d3cb91488cbea315d4278629e991e1644afa2d03
semyrekov.vladyslav <semyrekov.vladyslav@gmail.com>
2025-07-22 19:35:41 +0300

    last

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as dayjs from 'dayjs';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.less']
 })
 export class FooterComponent implements OnInit {
+
+  currentYear = { value: dayjs().year() };
 
   constructor() { }
 
